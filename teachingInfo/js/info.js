199aebbf8fa834b0e49e370de0b5ca14fb2693b1
刘东明 <itucas@foxmail.com>
2017-08-22 17:35:57 +0800

    ruiding

---
+++ teachingInfo/js/info.js
@@ -0,0 +1,12 @@
+function showcontent(language){
+	// $('#content').html(language);
+}
+
+function formatPrice(val,row){
+	console.log("val",val);
+	// if (val < 30){
+	// 	return '<span style="color:red;">('+val+')</span>';
+	// } else {
+	// 	return val;
+	// }
+}
